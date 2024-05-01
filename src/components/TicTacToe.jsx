e8c25f0372d13f766c9560032b732610c49527e5
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2024-05-01 15:03:38 +0530

    Prem | Add logic for winning condition

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -3,6 +3,10 @@ import './TicTacToe.css';
 import Cells from "./Cells";
 import Players from "./Players";
 
+const Winner = ({name}) => {
+  return <div className="winner-banner">Congratulations {name}!</div>;
+}
+
 class TicTacToe extends React.Component {
   constructor(props) {
     super(props);
@@ -13,12 +17,13 @@ class TicTacToe extends React.Component {
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
         [0, 3, 6], [1, 4, 7], [2, 5, 8],
         [0, 4, 8], [2, 4, 6]
-      ]
+      ],
+      isWinner: false,
     }
     this.handleMove = this.handleMove.bind(this);
   }
 
-  checkWinner(cells) {
+  isWinner(cells) {
     const playerSymbol = this.props.players[this.state.currentPlayerIndex].symbol;
     const playerMoves = cells.reduce((moves, cell, index) => {
       if (cell === playerSymbol) {
@@ -26,33 +31,41 @@ class TicTacToe extends React.Component {
       }
       return moves;
     }, []);
-    const isWinner = this.state.winningMoves.some((moves) => {
+    return this.state.winningMoves.some((moves) => {
       return moves.every((move) => playerMoves.includes(move));
-    });
+    })
+  }
 
-    this.setState({
-      winner: isWinner ? this.props.players[this.state.currentPlayerIndex].name : null
-    });
+  updateCells(cells) {
+    this.setState({cells: cells});
+  }
+
+  updateCurrentPlayerIndex(currentPlayerIndex) {
+    this.setState({currentPlayerIndex: currentPlayerIndex});
   }
 
   handleMove = (cellId) => {
+    if (this.state.isWinner) {
+      return;
+    }
     const cells = this.state.cells.slice();
     cells[cellId] = this.props.players[this.state.currentPlayerIndex].symbol;
-    this.checkWinner(cells);
-    this.setState({
-      cells: cells,
-      currentPlayerIndex: 1 - this.state.currentPlayerIndex,
-    });
+    let isWinner = this.isWinner(cells);
+    this.setState({isWinner: isWinner});
+    this.updateCells(cells);
+    if (isWinner) {
+      return;
+    }
+    this.updateCurrentPlayerIndex(1 - this.state.currentPlayerIndex);
   }
 
   render() {
+    const currentPlayer = this.props.players[this.state.currentPlayerIndex].name;
     return (<div className="game-layout">
           <div className="title">Tic Tac Toe</div>
           <Players players={this.props.players} currentPlayerIndex={this.state.currentPlayerIndex}/>
           <Cells cells={this.state.cells} handleMove={this.handleMove}/>
-          {
-            this.state.winner ? <div className="winner">Winner: {this.state.winner}</div> : null
-          }
+          {this.state.isWinner ? <Winner name={currentPlayer}/> : null}
         </div>
     );
   }
